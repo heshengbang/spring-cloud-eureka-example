676a4203f411f062a9311fdbe91187622b8c198b
heshengbang <yunshixin8521894@sohu.com>
2018-08-01 18:36:11 +0800

    add oauth-example

---
+++ oAuth-example/src/main/java/com/hsb/security/oAuthexample/OAuthExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hsb.security.oAuthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthExampleApplication.class, args);
+	}
+}
